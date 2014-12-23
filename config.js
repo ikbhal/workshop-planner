93a62dafef81b4ff55101f3a907d73e9d4aa2671
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2014-12-23 10:01:05 +0530

    Adding config.js, add npm modules

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth': {
+		'clientID' : '1509718452626797',
+		'clientSecret': '7fad3991fe0a6aff91350482ae279630',
+		'callbackURL': 'https://workshop-planner.herokuapp.com/auth/facebook/callback'
+	}
+};
