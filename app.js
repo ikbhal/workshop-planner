7b431a44b61e1be7cdf87115fdf86dfaaee01060
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2014-12-23 10:24:31 +0530

    Add passport serialize,deserilaize, config, app configure, app facebook auth , auth callback, account, / routes

--- app.js
+++ app.js
@@ -1,15 +1,89 @@
 var config = require('./oauth.js');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
 var express = require('express');
+
+// serialize and deserialize
+passport.serializeUser(function(user, done){
+	done(null, user);
+});
+
+passport.deserializeUser(function(obj, done){
+	done(null, obj);
+});
+
+// config
+passport.use(new FacebookStrategy({
+		clientID: config.facebook.clientID,
+		clientSecret: config.facebook.clientSecret,
+		callbackURL: config.facebook.callbackURL
+	},
+	function(accessToken, refreshToken, profile, done){
+		process.nextTick(function(){
+			return done(null, profile);
+		});
+	} 
+));
+
 var app = express();
 
-app.set('port', (process.env.PORT || 80));
-app.use(express.static(__dirname + '/public'));
+app.configure(function(){
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.logger());
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.session({secret: 'my_precious'}));
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(app.router);
+	//app.set('port', (process.env.PORT || 80));
+	app.use(express.static(__dirname + '/public'));
+});
+
+// routes
+app.get('/', routes.index);
+app.get('/ping', routes.ping);
+app.get('/account', ensureAuthenticated, function(req, res){
+	res.render('account', {user: req.user});
+});
+
+app.get('/', function(req, res){
+	res.render('login', {iser: user});
+});
+
+app.get('/auth/facebook', 
+	passport.authenticate('facebook'),
+	function(req, res){});
+
+app.get('auth/facebook/callback', 
+	passport.authenticate('facebook', {
+		failureRedirect: '/'
+	}), 
+	function(req, res){
+		res.redirect('/account');
+	});
+
+app.get('/logout', function(req, res){
+	req.logout();
+	res.redirect('/');
+});
 
 
+/*
 app.get('/', function(request, response) {
 	response.send('Hello World!');
 });
-
+*/
 app.listen(app.get('port'), function() {
 	console.log("Workshop Planner running at localhost:" + app.get('port'));
 });
+
+//test authentication
+function ensureAuthenticated(req, res, next) {
+	if(req.isAuthenticated()) {
+		return next();
+	} 
+	res.redirect('/');
+}
