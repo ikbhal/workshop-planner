e71b6e340686fc1d7872108570b1eeaac946cdfb
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2014-12-23 10:06:09 +0530

    Renaming index.js to app.js , config.js to oauth.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var config = require('./oauth.js');
 var express = require('express');
 var app = express();
 
--- oauth.js
+++ oauth.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'facebookAuth': {
+	'facebook': {
 		'clientID' : '1509718452626797',
 		'clientSecret': '7fad3991fe0a6aff91350482ae279630',
 		'callbackURL': 'https://workshop-planner.herokuapp.com/auth/facebook/callback'
