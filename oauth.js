ee578b84f5a43563cdf9e289e1368bf6cc9c4540
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2014-12-23 14:51:28 +0000

    Correcting callback url to toprecur.cloudapp.net

--- oauth.js
+++ oauth.js
@@ -2,6 +2,7 @@ module.exports = {
 	'facebook': {
 		'clientID' : '1509718452626797',
 		'clientSecret': '7fad3991fe0a6aff91350482ae279630',
-		'callbackURL': 'https://workshop-planner.herokuapp.com/auth/facebook/callback'
+		//'callbackURL': 'https://workshop-planner.herokuapp.com/auth/facebook/callback'
+		'callbackURL': 'http://toprecur.cloudapp.net/auth/facebook/callback'
 	}
 };
