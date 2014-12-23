9c96396350a23b011744350d8696feb20284c3d4
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2014-12-23 11:28:42 +0530

    use body-parser, method-override, morgan logger, serve-favicon, error-handler , removed app.configure, app.routes

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var http = require('http');
-//var path = require('path');
+var path = require('path');
 //var fs = require('fs');
+var express = require('express');
+var logger = require('morgan');
+var favicon = require('serve-favicon');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var errorHandler = require('errorhandler');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+
+
 var config = require('./oauth.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
-var express = require('express');
 var routes = require('./routes');
 
 // serialize and deserialize
@@ -32,20 +41,24 @@ passport.use(new FacebookStrategy({
 
 var app = express();
 
-app.configure(function(){
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	app.use(express.logger());
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(express.session({secret: 'my_precious'}));
-	app.use(passport.initialize());
-	app.use(passport.session());
-	app.use(app.router);
-	app.set('port', (process.env.PORT || 80));
-	app.use(express.static(path.join(__dirname , 'public')));
-  app.use(express.errorHandler({dumpExceptions:true, showStack:true}));
-});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+//app.use(express.logger());
+//app.use(express.bodyParser());
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(session({resave:true, saveUninitialized:true, secret: 'uwotm8'}));
+
+//app.use(express.session({secret: 'my_precious'}));
+app.use(passport.initialize());
+app.use(passport.session());
+//app.use(app.router);
+app.set('port', (process.env.PORT || 80));
+app.use(express.static(path.join(__dirname , 'public')));
+app.use(errorHandler({dumpExceptions:true, showStack:true}));
 
 // mongo config
 //var MONGOLAB_URI= "add_your_mongolab_uri_here"
